3b8ca001f197a0bc05c9bd3614d3c0233d665d29
Bergi-e <jayden.bergstrome@gmail.com>
2025-05-19 16:04:13 -0700

    Added a few functions to check if the two cards are the same, some bugs to fix.

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 function setup () {
-  let firstCard = undefined
-  let secondCard = undefined
+  let firstCard = null;
+  let secondCard = null;
+  let preventClick = false;
+
+  function checkForMatch() {
+    console.log("Running checkForMatch");
+    if (firstCard.src === secondCard.src) {
+      disableMatchedCards();
+    } else {
+      unflipCards();
+    }
+  }
+
+  function disableMatchedCards() {
+    console.log("Running disableMatchedCards");
+    $(`#${firstCard.id}`).parent().off('click');
+    $(`#${secondCard.id}`).parent().off('click');
+    pairsMatched++;
+    resetSelection();
+  }
+
+  function unflipCards() {
+    console.log("Running unflipCards");
+    setTimeout(() => {
+      $(`#${firstCard.id}`).parent().toggleClass('flip');
+      $(`#${secondCard.id}`).parent().toggleClass('flip');
+      resetSelection();
+    }, 1000);
+  }
+
+  function resetSelection() {
+    console.log("Running resetSelection");
+    firstCard = null;
+    secondCard = null;
+    preventClick = false;
+  }
+
   $(".card").on(("click"), function () {
     $(this).toggleClass("flip");
 
@@ -8,22 +43,8 @@ function setup () {
       firstCard = $(this).find(".front_face")[0]
     else {
       secondCard = $(this).find(".front_face")[0]
-      console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      ) {
-        console.log("match")
-        $(`#${firstCard.id}`).parent().off("click")
-        $(`#${secondCard.id}`).parent().off("click")
-      } else {
-        console.log("no match")
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
-        }, 1000)
-      }
+      preventClick = true;
+      checkForMatch();
     }
   });
 }
