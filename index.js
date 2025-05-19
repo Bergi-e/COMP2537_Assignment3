1f561b98d1641865a350bcd72af88655fac7dba0
Bergi-e <jayden.bergstrome@gmail.com>
2025-05-19 16:43:47 -0700

    Fixed bug where I could interact with a card that shouldn't be interactable

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ function setup () {
 
   function disableMatchedCards() {
     console.log("Running disableMatchedCards");
-    $(`#${firstCard.id}`).parent().off('click');
-    $(`#${secondCard.id}`).parent().off('click');
-    pairsMatched++;
+    $(`#${firstCard.id}`).parent().off('click').addClass('matched');
+
+    $(`#${secondCard.id}`).parent().off('click').addClass('matched');
     resetSelection();
   }
 
   function unflipCards() {
     console.log("Running unflipCards");
     setTimeout(() => {
-      $(`#${firstCard.id}`).parent().toggleClass('flip');
-      $(`#${secondCard.id}`).parent().toggleClass('flip');
+      $(`#${firstCard.id}`).parent().removeClass('flip');
+      $(`#${secondCard.id}`).parent().removeClass('flip');
       resetSelection();
     }, 1000);
   }
@@ -36,15 +36,20 @@ function setup () {
     preventClick = false;
   }
 
-  $(".card").on(("click"), function () {
-    $(this).toggleClass("flip");
+$(".card").on("click", function () {
+  const $card = $(this);
+  if (preventClick || $card.hasClass("matched")) return;
+  const currentCard = $card.find(".front_face")[0];
+  if (firstCard && currentCard === firstCard) return;
 
-    if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
-    else {
-      secondCard = $(this).find(".front_face")[0]
-      preventClick = true;
-      checkForMatch();
+  $card.addClass("flip");
+
+  if (!firstCard) {
+    firstCard = currentCard;
+  } else {
+    secondCard  = currentCard;
+    preventClick = true;
+    checkForMatch();
     }
   });
 }
