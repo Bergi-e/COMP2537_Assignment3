8e0cdca4fb283826f873649f028609f15e6de7cb
Bergi-e <jayden.bergstrome@gmail.com>
2025-05-19 17:10:22 -0700

    Added proper winning message within the page itself

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ function setup () {
     $(`#${firstCard.id}`).parent().off('click').addClass('matched');
     $(`#${secondCard.id}`).parent().off('click').addClass('matched');
     pairsMatched++;
-    if (pairsMatched === totalPairs) {
-      alert("You win!");
-    }
+    if (pairsMatched === totalPairs) endGame();
     resetSelection();
   }
 
@@ -41,22 +39,28 @@ function setup () {
     preventClick = false;
   }
 
-$(".card").on("click", function () {
-  const $card = $(this);
-  if (preventClick || $card.hasClass("matched")) return;
-  const currentCard = $card.find(".front_face")[0];
-  if (firstCard && currentCard === firstCard) return;
+  function endGame() {
+    preventClick = true;                     
+    const endMsg = 'You Win!!!';
+    $('#message').text(endMsg).show();
+  }
 
-  $card.addClass("flip");
+  $(".card").on("click", function () {
+    const $card = $(this);
+    if (preventClick || $card.hasClass("matched")) return;
+    const currentCard = $card.find(".front_face")[0];
+    if (firstCard && currentCard === firstCard) return;
 
-  if (!firstCard) {
-    firstCard = currentCard;
-  } else {
-    secondCard  = currentCard;
-    preventClick = true;
-    checkForMatch();
-    }
-  });
-}
+    $card.addClass("flip");
+
+    if (!firstCard) {
+      firstCard = currentCard;
+    } else {
+      secondCard  = currentCard;
+      preventClick = true;
+      checkForMatch();
+      }
+    });
+  }
 
 $(document).ready(setup)
