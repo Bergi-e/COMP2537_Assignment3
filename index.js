a67aa31de437781e12244a70f7708d0d4b34621b
Bergi-e <jayden.bergstrome@gmail.com>
2025-05-19 19:13:45 -0700

    Implemented a timer to the header, you now lose if you run out of time

--- index.js
+++ index.js
@@ -5,14 +5,43 @@ function setup () {
   let pairsMatched = 0;
   let totalPairs = 3; // Default
   let clicks = 0;
+  let timer = 5; // Default
+  let gameTimer;
 
   function updateClicks() {
+    console.log("Running updateClicks");
     $("#clicks").text(clicks);
   }
 
   function updatePairsMatched() {
+    console.log("Running updatePairsMatched");
     $("#pairs_matched").text(pairsMatched);
     $("#pairs_left").text(totalPairs - pairsMatched);
+  }
+  
+  function startTimer() {
+    console.log("Running startTimer");
+    updateTimer();
+    gameTimer = setInterval(() => {
+      timer--;
+      updateTimer();
+      if (timer <= 0) {
+        clearInterval(gameTimer);
+        outOfTime();
+      }
+    }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(gameTimer);
+  }
+
+  function updateTimer() {
+    console.log("Running updateTimer");
+    const mins = Math.floor(timer / 60);
+    const secs = timer % 60;
+    const padded = secs.toString().padStart(2, '0');
+    $("#timer").text(`${mins}:${padded}`);
   }
 
   function checkForMatch() {
@@ -52,12 +81,28 @@ function setup () {
     preventClick = false;
   }
 
+  function outOfTime() {
+    console.log("Running outOfTime");
+    preventClick = true;
+    const endMsg = 'Time is up!!!';
+    $('#message').text(endMsg).show();
+  }
+
   function endGame() {
-    preventClick = true;                     
+    console.log("Running endGame");
+    preventClick = true;        
+    stopTimer();             
     const endMsg = 'You Win!!!';
     $('#message').text(endMsg).show();
   }
 
+  // Initial header setup
+  updateClicks();
+  updatePairsMatched();
+  updateTimer();
+  startTimer();
+
+  // Card clicking
   $(".card").on("click", function () {
     const $card = $(this);
     if (preventClick || $card.hasClass("matched")) return;
